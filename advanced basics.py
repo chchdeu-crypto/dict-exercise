6480557c52971fb4f77f0a628ad88f6c73693bfb
chaim <chchdeu@gmail.com>
2026-06-23 13:52:55 +0300

    mission_2

---
+++ advanced basics.py
@@ -0,0 +1,11 @@
+#mission 1
+config= {}
+config.setdefault("timeout",30)
+print(config)
+config.setdefault("timeout",31)
+print(config)
+#mission 2
+d1={"a":1,"b":2}
+d2={"b":3,"c":4}
+merged=d1 | d2
+print(merged)
